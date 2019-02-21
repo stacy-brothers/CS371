b39d5d1b3563a07c5ef002fbe55a55dca9d354de
Stacy Brothers <bro18063@byui.edu>
2019-02-20 21:54:09 -0700

    adding google map

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+/// <reference types="@types/googlemaps" />
+import {Component, OnInit, ViewChild} from '@angular/core';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'map-app';
+export class AppComponent implements OnInit  {
+  @ViewChild('gmap') gmapElement: any;
+  map: google.maps.Map;
+
+  ngOnInit() {
+    const mapProp = {
+      center: new google.maps.LatLng(18.5793, 73.8143),
+      zoom: 15,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+    this.map = new google.maps.Map(this.gmapElement.nativeElement, mapProp);
+  }
 }
